c9e85a635efe2735435d9ad76e11ed57be216810
Genta <hutika.nico@gmail.com>
2021-02-15 13:49:11 +0900

    更新デプロイチェック

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Header from "../components/header"
 export default function Home() {
   return <div style={{ color: `purple`, fontSize: `72px` }}>
     <Link to="/contact/">Contact</Link>
+    <Link to="/about/">About</Link>
     <a href="https://github.com/genlivre" target="_blank">github</a>
     <Header headerText="Gatsby Testing Page!" />
     <p>User name is Genta.</p>
