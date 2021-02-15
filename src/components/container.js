71566b9256fee945570834462808b72a93be74d5
Genta <hutika.nico@gmail.com>
2021-02-15 15:25:33 +0900

    tutorial2 Style（CSS, Scss）の追加

---
+++ src/components/container.js
@@ -0,0 +1,5 @@
+import React from "react"
+import containerStyles from "./container.module.scss"
+export default function Container({ children }) {
+  return <div className={containerStyles.container}>{children}</div>
+}
